3f3df680c9adaa42e1508136aaf3619314add021
donnywonny2025 <donnywonny2023@gmail.com>
2025-09-14 00:46:29 -0400

    Use Node.js Server for Text Display

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'text-display.html');
+  
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('File not found');
+      return;
+    }
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(content);
+  });
+});
+
+const PORT = 3001;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
